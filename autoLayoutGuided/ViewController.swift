9232275221e4ae1a0fc4e42dd2ee8ed15603b75f
Jarren Campos <jarrenmcampos@gmail.com>
2020-01-30 18:45:13 -0800

    Initialize

---
+++ autoLayoutGuided/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autoLayoutGuided
+//
+//  Created by Jarren Campos on 1/30/20.
+//  Copyright © 2020 Jarren Campos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
